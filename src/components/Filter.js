9acf09c0c6afdc041304f37f30f7936c3285552d
Matthew Bomett <bomettmat2@gmail.com>
2023-10-31 19:44:49 +0300

    partial completion

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, onSearchChange }) {
+  // initialzing the state for the search text
+  const [searchText, setSearchText] = useState('')
+
+  // function to handle changes in the search input
+  const handleSearchChange = (e) => {
+    // updating the state with the new search text
+    setSearchText(e.target.value)
+    // calling te onSearchChange function passed as a prop
+    onSearchChange(e.target.value)
+  }
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input type="text" name="search" placeholder="Search..." value={searchText} onChange={handleSearchChange}// setting up an event handler for input changes
+      />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
+import items from "../data/items";
 
-function ItemForm(props) {
+function ItemForm({onItemFormSubmit}) {
+  // initializing state for the item name and category
+  const [itemName, setItemName] = useState('')
+  const [itemCategory, setItemCategory] = useState('')
+
+  // function to handle changes in the item name input
+  const handleChangeName = (e) => {
+    setItemName(e.target.value)
+  }
+
+  // function to handle changes in the category select
+  const handleCategoryChange = (e) => {
+    setItemCategory(e.target.value)
+  }
+
+  // function to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // createe a new item
+    const newItem = {
+      id: uuid(),
+      name: itemName,
+      category: itemCategory,
+    }
+
+    // calling the onItemFormSubmit function passed as a prop
+    onItemFormSubmit(newItem)
+
+    // clearing input fields
+    setItemName('')
+    setItemCategory('')
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={itemName} onChange={handleChangeName}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" value={itemCategory} onChange={handleCategoryChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
